f1141c51088ec77519396cd3b32c83e31a600a34
Khush1694 <132439373+Khush1694@users.noreply.github.com>
2024-02-07 17:57:23 +0530

    Create slack.php

---
+++ slack.php
@@ -0,0 +1,4 @@
+private readonly LoggerInterface $logger,        
+private readonly UserDeletedService $userDeletedService,
+private readonly PlatformManagerInterface $platformManager,
+private readonly SlackClientInterface $slackClient
